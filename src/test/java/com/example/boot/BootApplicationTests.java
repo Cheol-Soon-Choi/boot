afc077defbd6be08e5e8fe437c0c5c13e270fa0b
Cheol-Soon-Choi <79562672+Cheol-Soon-Choi@users.noreply.github.com>
2021-10-25 13:47:57 +0900

    Add test.java

---
+++ src/test/java/com/example/boot/BootApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootTest
+class BootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/com/example/boot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,22 @@
+package com.example.boot.web.dto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void 롬복_기능_테스트(){
+        //given
+        String name = "test";
+        int amout = 1000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amout);
+
+        //then
+        assertThat(dto.getName()).isEqualTo("test");
+        assertThat(dto.getAmount()).isEqualTo(amout);
+    }
+}
